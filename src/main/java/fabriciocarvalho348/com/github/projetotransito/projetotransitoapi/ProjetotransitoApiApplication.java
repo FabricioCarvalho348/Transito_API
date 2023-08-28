e967935562b07f4f99284fa584ab557b9cfb4071
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2023-08-28 15:58:44 -0300

    starting the project of an API for traffic administration in Java and Spring

---
+++ src/main/java/fabriciocarvalho348/com/github/projetotransito/projetotransitoapi/ProjetotransitoApiApplication.java
@@ -0,0 +1,13 @@
+package fabriciocarvalho348.com.github.projetotransito.projetotransitoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotransitoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fabriciocarvalho348/com/github/projetotransito/projetotransitoapi/ProjetotransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fabriciocarvalho348.com.github.projetotransito.projetotransitoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
